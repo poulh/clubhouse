5e19a8469e46496a6d4ad95b651ad1441f4c4762
Poul Hornsleth <poulh@umich.edu>
2018-09-15 09:47:11 -0400

    First cut at creating event off of the account. more work to do

--- server/boot/sample_checkin_data.js
+++ server/boot/sample_checkin_data.js
@@ -7,37 +7,36 @@ module.exports = function (app) {
             throw (err);
         }
 
-        console.log("account and user created. login!");
-        //        console.log(token.users());
+        Client = app.models.Client;
+        Client.findById(token.userId, function (err, client) {
 
+            var Event = app.models.Event;
+            var today = new Date();
 
-        var Event = app.models.Event;
-        var today = new Date();
+            Event.create({ name: "Club Event #1", date: today, accountId: client.accountId }, function (err, eventInstance) {
+                console.log(eventInstance);
+            });
 
-        Event.create({ name: "Club Event #1", date: today, accountId: 1 }, function (err, eventInstance) {
-            console.log(eventInstance);
+            today.setMonth(today.getMonth() - 1);
+            Event.create({ name: "Club Event #2", date: today, accountId: client.accountId, locked: true }, function (err, eventInstance) {
+                console.log(eventInstance);
+            });
+
+            var Member = app.models.Member;
+
+            Member.create({ firstName: "Jane", lastName: "ClubMember", email: "jane@jane.com", cellPhone: "212-555-1234" }, function (err, memberInstance) {
+                console.log(memberInstance);
+            });
+
+            Member.create({ firstName: "Jim", lastName: "Guest", email: "jim@jim.com", cellPhone: "212-555-5678" }, function (err, memberInstance) {
+                console.log(memberInstance);
+            });
+
+            Member.create({ firstName: "Larry", lastName: "Lifetime", email: "larry@larry.com", cellPhone: "212-555-9999" }, function (err, memberInstance) {
+                console.log(memberInstance);
+            });
         });
 
-        //var Event = app.models.Event;
-        //var today = new Date();
-        today.setMonth(today.getMonth() - 1);
-        Event.create({ name: "Club Event #2", date: today, accountId: 1, locked: true }, function (err, eventInstance) {
-            console.log(eventInstance);
-        });
-
-        var Member = app.models.Member;
-
-        Member.create({ firstName: "Jane", lastName: "ClubMember", email: "jane@jane.com", cellPhone: "212-555-1234" }, function (err, memberInstance) {
-            console.log(memberInstance);
-        });
-
-        Member.create({ firstName: "Jim", lastName: "Guest", email: "jim@jim.com", cellPhone: "212-555-5678" }, function (err, memberInstance) {
-            console.log(memberInstance);
-        });
-
-        Member.create({ firstName: "Larry", lastName: "Lifetime", email: "larry@larry.com", cellPhone: "212-555-9999" }, function (err, memberInstance) {
-            console.log(memberInstance);
-        });
     });
 
 
