7d1586185019a6107e432c4808327e98bf2e168c
Poul Hornsleth <poulh@umich.edu>
2018-09-17 08:28:53 -0400

    First cut at forcing all Event/Member queries to be by the user's accountId, not userid.

--- common/models/event.js
+++ common/models/event.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Event) {
+module.exports = function (Event) {
+    Event.observe('access', async function (ctx, next) {
 
+        if (ctx && ctx.options && ctx.options.accessToken && ctx.options.accessToken.userId) {
+            const userId = ctx.options.accessToken.userId;
+
+            let RegisteredUser = Event.app.models.RegisteredUser;
+            RegisteredUser.findById(userId, function (err, user) {
+                ctx.query.where = Object.assign({}, ctx.query.where, { accountId: user.accountId })
+            });
+        }
+
+        return;
+    });
 };
--- common/models/member.js
+++ common/models/member.js
@@ -1,5 +1,17 @@
 'use strict';
 
-module.exports = function(Member) {
+module.exports = function (Member) {
+    Member.observe('access', async function (ctx, next) {
 
+        if (ctx && ctx.options && ctx.options.accessToken && ctx.options.accessToken.userId) {
+            const userId = ctx.options.accessToken.userId;
+
+            let RegisteredUser = Member.app.models.RegisteredUser;
+            RegisteredUser.findById(userId, function (err, user) {
+                ctx.query.where = Object.assign({}, ctx.query.where, { accountId: user.accountId })
+            });
+        }
+
+        return;
+    });
 };
