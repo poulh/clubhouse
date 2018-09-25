4c882dc5dcee9b7eabbd03b2174f0facf208b908
Poul Hornsleth <poulh@umich.edu>
2018-09-25 19:54:31 -0400

    members page same look and feel as checkin. checkin should be derived from members.

--- client/src/app/members/members.component.ts
+++ client/src/app/members/members.component.ts
@@ -13,18 +13,58 @@ export class MembersComponent implements OnInit {
 
   isLoading = false;
   members: Member[];
+  sortOrder: [string, boolean][] = [];
+  search: string;
 
   constructor(private router: Router,
-    private memberApi: MemberApi) { }
+    private memberApi: MemberApi) {
+    this.search = "";
+    this.sortOrder = [["lastName", true], ["firstName", true]];
+  }
 
   ngOnInit() {
     this.getMembers();
   }
 
+  sort(field: string): void {
+    /*
+     If clicked field is already first it flips the asc/desc.
+     If clicked field is not first, move it to first and set everything asc
+     */
+    let firstItem = this.sortOrder[0];
+    if (field == firstItem[0]) {
+      firstItem[1] = !firstItem[1];
+    } else {
+      const item = this.sortOrder.find(i => i[0] == field);
+      const index = this.sortOrder.indexOf(item);
+      this.sortOrder.splice(index);
+      this.sortOrder.unshift(item);
+      this.sortOrder.forEach(i => i[1] = true);
+    }
+    this.getMembers();
+  }
+
+  clearSearch(): void {
+    this.search = "";
+  }
+
+  displayMember(member: Member): boolean {
+    if (this.search.length == 0) {
+      return true;
+    }
+
+    return (member.firstName && member.firstName.toLowerCase().includes(this.search)) ||
+      (member.lastName && member.lastName.toLowerCase().includes(this.search)) ||
+      (member.email && member.email.toLowerCase().includes(this.search)) ||
+      (member.cellPhone && member.cellPhone.toLowerCase().includes(this.search));
+  }
+
   getMembers(): void {
     this.isLoading = true;
+
+    const orderQueryString = this.sortOrder.map(i => (i[0] + " " + (i[1] ? "ASC" : "DESC"))).join(", ");
     const filter = {
-      order: "lastName ASC, firstName ASC"
+      order: orderQueryString
     };
     this.memberApi.find<Member>(filter).subscribe(members => {
       this.members = members;
--- client/src/app/members/members.module.ts
+++ client/src/app/members/members.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { MembersRoutingModule } from './members-routing.module';
 import { MembersComponent } from './members.component';
@@ -7,6 +8,7 @@ import { MembersComponent } from './members.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     MembersRoutingModule
   ],
   declarations: [MembersComponent]
